d944b3b845a6ebf3b1941ecf8f02870f256cf5e8
qubeczek <qubeczek1@gmail.com>
2017-10-15 16:39:01 +0200

    rolety wersja 1

--- custom_components/notify/modbushas.py
+++ custom_components/notify/modbushas.py
@@ -18,7 +18,7 @@ EVENT_NOTIFY = "notify"
 ATTR_ADDRESS = "address"
 ATTR_VALUE = "value"
 
-def get_service(hass, config):
+def get_service(hass, config, discovery_info=None):
     """Get the ModbusHAS notification service."""
     return ModbusHASNotificationService(hass,1)
 
